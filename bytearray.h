50c566563a46791acd3aa135cc28821a46cb4611
dg4l <79771590+dg4l@users.noreply.github.com>
2025-01-31 12:16:31 -0500

    add write function and clean up names

--- bytearray.h
+++ bytearray.h
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 typedef struct{
     uint8_t* buf;
-    size_t filesize;
+    size_t bufsize;
 }ByteArray; 
 
 size_t get_file_size(FILE* f){
@@ -16,7 +18,7 @@ size_t get_file_size(FILE* f){
 }
 
 void print_byte_array(ByteArray* ba){
-    for (int i = 0; i < ba->filesize; ++i){
+    for (int i = 0; i < ba->bufsize; ++i){
         if (!(i % 16)) printf("\n");
         printf("%02X ", ba->buf[i]);
     }
@@ -28,21 +30,29 @@ void cleanup_bytearray(ByteArray **ba){
     *ba = NULL;
 }
 
-ByteArray* move_file_into_byte_array(char* filename){
+bool byte_array_to_file(ByteArray* ba, char* filename){
+    FILE* fp = fopen(filename, "wb");
+    if (!fp) return 0;
+    fwrite(ba->buf, 1, ba->bufsize, fp);
+    fclose(fp);
+    return 1;
+}
+
+ByteArray* file_to_byte_array(char* filename){
     FILE* f = fopen(filename, "rb");
     if (!f){
         printf("ERROR: FILE NOT FOUND\n");  
         return NULL;
     }
     ByteArray* b = malloc(sizeof(ByteArray));
-    b->filesize = get_file_size(f);
-    b->buf = malloc(b->filesize * sizeof(uint8_t));
+    b->bufsize = get_file_size(f);
+    b->buf = malloc(b->bufsize * sizeof(uint8_t));
     if (!b->buf){
         printf("ERROR: MEMORY COULD NOT BE ALLOCATED\n");
         fclose(f);
         return NULL;
     }
-    fread(b->buf, 1, b->filesize, f);
+    fread(b->buf, 1, b->bufsize, f);
     fclose(f);
     return b;
 }
